19a0ce46430eeb6b7cf32d40a7775381e36d75e5
emilime93 <emil93@gmail.com>
2018-04-24 10:56:57 +0200

    renamed classes and removed placeholder section

--- website/view/pages/logged_out.php
+++ website/view/pages/logged_out.php
@@ -1,23 +1,12 @@
 <div id="main">
 		<h1>Welcome to DigiBoards site</h1>
-		<div class="section1">
+		<div class="section">
 			<h1>Messages</h1>
-			<div class='comment-box'>
-				<p>
-					<p class='user-date'><br>Kalle</p>
-					2018-04-13 10:30 <br> Inte på kontoret
-				</p>
-			</div>
-			<div class='comment-box'>
-                <p>
-                    <p class='user-date'><br>Olle</p>
-                    2018-04-10 18:05 <br> På semester!!!
-                </p>
-			</div>
+			<p>Welcome to out amazing site.</p>
 		</div>
-		<div class="section2">
+		<div class="section">
 			<h1>Register</h1>
-			<form action="controller/user.php" method="POST">
+			<form id="registry-form" action="controller/user.php" method="POST">
 				<input type="text" name="first" placeholder="Firstname">
 			    <input type="text" name="last" placeholder="Lastname">
 			    <input type="text" name="username" placeholder="Username">

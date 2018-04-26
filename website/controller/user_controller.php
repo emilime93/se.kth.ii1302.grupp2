75de7563a3e72c87a0616d54b4ae81c1dc00e891
jmatss <jmatss@kth.se>
2018-04-26 14:53:54 +0200

    Fixat user_controller till objekt

--- website/controller/user_controller.php
+++ website/controller/user_controller.php
@@ -1,24 +1,12 @@
 <?php
-session_start();
-
-switch($_POST['button']) {
-	case "register":
-		$username = $_POST['username'];
-		$password = \password_hash($_POST['password'], PASSWORD_DEFAULT);
-		$signup_code = $_POST['signup_code'];
-		$fname = $_POST['fname'];
-		$lname = $_POST['lname'];
-		$email = $_POST['email'];
-		
+class UserController {
+	function create_user($registry_DTO) {
 		//  the if statement is true if username contains chars that isn't letters, numbers or in "$valid_chars"
 		$valid_chars = array('_', '-');
 		if (!\ctype_alnum(\str_replace($valid_chars, '', $username))) {
 			$_SESSION['register_success'] = false;
 			header("Location: /index.php");
 		}
-		
-		require_once('../modelDTO/registry_DTO.php');
-		$registry_DTO = new registry_DTO($username, $password, $signup_code, $fname, $lname, $email);
 		
 		require_once('../integration/user_db.php');
 		$user_db = new user_db();
@@ -29,14 +17,8 @@ switch($_POST['button']) {
 			$_SESSION['register_success'] = false;
 		}
 		header("Location: /index.php");
-	break;
-	case "login":
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		
-		require_once('../modelDTO/user_DTO.php');
-		$user_DTO = new user_DTO($username, $password);
-		
+	}
+	function login_user($user_DTO) {
 		require_once('../integration/user_db.php');
 		$user_db = new user_db();
 		
@@ -47,8 +29,5 @@ switch($_POST['button']) {
 			$_SESSION['login_failed'] = true;
 		}
 		header("Location: /index.php");
-	break;
-	default:
-		header("Location: /index.php");
-	break;
+	}
 }

5c536352871bf101fe374aa7b843bf221d8c6abf
emilime93 <emil93@gmail.com>
2018-04-26 13:23:44 +0200

    Backbone logged in view and css created

--- website/view/pages/logged_in.php
+++ website/view/pages/logged_in.php
@@ -1,2 +1,24 @@
-<?php
-echo "Logged IN";
+<main>
+    <section id="banner">
+        <h2>Currently displayed Message</h2>
+        <p>Hej hej monika hej på dig monika!</p>
+    </section>
+    <section>
+        <h2>Saved Messages</h2>
+        <div class="message">
+            <p>Jag VABBAR</p>
+        </div>
+        <div class="message">
+            <p>Fast i Trafik</p>
+        </div>
+        <div class="message">
+            <p>Skolkar (hehe)</p>
+        </div>
+    </section>
+    <section>
+        <h2>Enter Message</h2>
+        <form id="enter_message_form" action="../../controller/message_controller.php" method="post"></form>
+        <p>Enter Message</p>
+        <textarea form="enter_message_form" name="message" id="" cols="30" rows="10"></textarea>
+    </section>
+</main>
--- website/view/pages/logged_out.php
+++ website/view/pages/logged_out.php
@@ -1,11 +1,11 @@
 <main class="clearfix">
 		<h1>Welcome to DigiBoards site</h1>
 		<section>
-			<h1>Messages</h1>
+			<h2>Messages</h2>
 			<p>Welcome to out amazing site.</p>
 		</section>
 		<section>
-			<h1>Register</h1>
+			<h2>Register</h2>
 			<form id="registry-form" action="../../controller/user_controller.php" method="POST">
 				<input type="text" name="fname" placeholder="Firstname">
 			    <input type="text" name="lname" placeholder="Lastname">
